305a059458dba7c7857a43b12bb522ad71962f5c
Boo-seon-woong <boosunung@gmail.com>
2024-03-11 19:44:34 +0900

    hello react

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello react and npx!</h1>
+        <p>first time handling react_11 Mar 2024</p>
         <a
           className="App-link"
           href="https://reactjs.org"
