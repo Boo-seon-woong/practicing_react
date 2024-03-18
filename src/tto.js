3ed4975e5b0b9feaef0b99a48125c5b2639fadab
Boo-seon-woong <boosunung@gmail.com>
2024-03-18 20:49:48 +0900

    Board

--- src/tto.js
+++ src/tto.js
@@ -5,28 +5,41 @@ import './App.css';
 import userEvent from '@testing-library/user-event';
 import { useState } from 'react';
 
-function Square(){
-    return <button className='square'>x</button>
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue('X');
+    }
+  
+    return (
+      <button className="square" onPointerMove={handleClick} >
+        {value}
+      </button>
+    );
 }
 
 function Board(){
     return(
         <>
         <div className="board-row">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
-        </div>
-        <div className="board-row">
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
-        </div>
-        <div className="board-row">
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
-        </div>
+      {[...Array(4000)].map((value, index) => {  
+        return (
+          <Square/>  
+        );  
+      })}  
+
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
       </>
     )
 }
