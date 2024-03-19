8e759640048b0a0b4e165c6bcf29d7fe63291a0b
Boo-seon-woong <boosunung@gmail.com>
2024-03-19 19:04:10 +0900

    now the function is on the board

--- src/tto.js
+++ src/tto.js
@@ -1,47 +1,41 @@
-import logo from './logo.svg';
-import {ScreenMap,CombinedMyButton} from './func';
-import {user,products} from './data';
-import './App.css';
-import userEvent from '@testing-library/user-event';
 import { useState } from 'react';
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X');
-    }
-  
-    return (
-      <button className="square" onPointerMove={handleClick} >
-        {value}
-      </button>
-    );
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
-function Board(){
-    return(
-        <>
-        <div className="board-row">
-      {[...Array(4000)].map((value, index) => {  
-        return (
-          <Square/>  
-        );  
-      })}  
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-      </>
-    )
+    </>
+  );
 }
 
 export {Square,Board};
