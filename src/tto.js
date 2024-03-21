ab979da7ec29cb81e1c4aba0aea6c38b3a9ff171
Boo-seon-woong <boosunung@gmail.com>
2024-03-21 21:24:14 +0900

    now tictacto is aligned as center

--- src/tto.js
+++ src/tto.js
@@ -25,20 +25,27 @@ export default function Board() {
 
   return (
     <>
+      <h1 style={{color:"white"}}>내맘대로 만드는 틱택토 by BOO SEONWOONG</h1>
       <div className="board-row">
+        <div className='cover'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
       </div>
       <div className="board-row">
+        <div className='cover'>
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
       </div>
       <div className="board-row">
+        <div className='cover'>
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
       </div>
     </>
   );
